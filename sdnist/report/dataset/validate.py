121ca375b33c9b6cd587621adf15f3b218e97fb8
Karan Bhagat <kb.in.triangulum@gmail.com>
2025-07-16 16:34:11 +0530

    Update validation to correctly check if values '001' is equal to '1'. This supports validation in new dataset where many code values are left padded with 0s.

--- sdnist/report/dataset/validate.py
+++ sdnist/report/dataset/validate.py
@@ -4,7 +4,7 @@ import pandas as pd
 from sdnist.report.report_data import ValidationData
 from sdnist.report.dataset.transform import deduce_code_type
 from sdnist.report.dataset.data_dict import (
-    get_feature_type, dtype_to_python_type, safe_isnan)
+    get_feature_type, dtype_to_python_type, safe_isnan, parse_numeric_value)
 from sdnist.utils import SimpleLogger
 import sdnist.strs as strs
 
@@ -17,7 +17,8 @@ def console_out(log: SimpleLogger, text: str):
 def validate_categorical_feature(
     data: pd.DataFrame,
     data_dict: Dict[str, any],
-    feature: str
+    feature: str,
+    code_type: type,
 ):
     d = data
     f = feature
@@ -39,6 +40,13 @@ def validate_categorical_feature(
     f_uniques = d[f].astype(str).unique().tolist()
     f_uniques = ['nan' if safe_isnan(v) else v
                  for v in f_uniques]
+    # If all values are parsed as code_type,
+    # then convert them to code_type
+    val_types = [type(parse_numeric_value(v)) == code_type
+                 for v in f_vals]
+    if all(val_types):
+        f_vals = [code_type(v) for v in f_vals]
+        f_uniques = [code_type(v) for v in f_uniques]
     vob_values = list(set(f_uniques) - set(f_vals))
     return vob_values
 
@@ -67,10 +75,11 @@ def validate(data: pd.DataFrame,
              data_dict: Dict,
              features: List[str],
              log: Optional[SimpleLogger] = None):
-    "Remove columns with invalid values."
+    """Remove columns with invalid values."""
+
     validation_log = dict()
     d = data.copy()
-    vob_features = []   # features with out of bound values
+    vob_features = []   # features with out-of-bound values
     for f in features:
         f_type = get_feature_type(data_dict, f)
         dc_type = deduce_code_type(f, data_dict)
@@ -79,7 +88,7 @@ def validate(data: pd.DataFrame,
                 d[f] = pd.to_numeric(d[f], errors='coerce').round().astype('Int64').astype(
                     object).fillna(d[f].astype(str))
         if f_type == strs.CATEGORICAL:
-            vob_vals = validate_categorical_feature(d, data_dict, f)
+            vob_vals = validate_categorical_feature(d, data_dict, f, dc_type)
         else:
             vob_vals = validate_continuous_feature(d, data_dict, f)
         if len(vob_vals):
@@ -97,114 +106,3 @@ def validate(data: pd.DataFrame,
     validation_log = dict(vob_features)
 
     return d, validation_log
-
-
-# def validate_old(synth_data: pd.DataFrame,
-#              data_dict: Dict,
-#              features: List[str],
-#              log: Optional[SimpleLogger] = None):
-#     """
-#     Removes all columns with the out of bound values.
-#     """
-#     def console_out(text: str):
-#         if log is not None:
-#             log.msg(text, level=3, timed=False, msg_type='error')
-#
-#     validation_log = dict()
-#     sd = synth_data.copy()
-#     vob_features = []  # value out of bound
-#     nan_df = sd[sd.isna().any(axis=1)]
-#     nan_features = []
-#     if len(nan_df):
-#         nan_features = sd.columns[sd.isna().any()].tolist()
-#         validation_log['nans'] = {
-#             "nan_records": len(nan_df),
-#             "nan_features": nan_features
-#         }
-#         # sd = sd.dropna()
-#         # console_out(f'Found {len(nan_df)} records with NaN values. '
-#         #             f'Removed records with NaN values.')
-#
-#     for f in features:
-#         # check feature has out of bound value
-#         f_data = data_dict[f]
-#         has_nan = f in nan_features
-#         has_N = 'N' in f_data['values'] if f != 'INDP' else True
-#         f_vals = f_data['values'] if "values" in f_data else []
-#
-#         if 'min' in f_vals:
-#             fd = sd[[f]].copy()
-#             mask = fd[fd[f] != 'N'].index if has_N else fd.index
-#             fd.loc[mask, f] = pd.to_numeric(fd.loc[mask, f], errors="coerce")
-#             nans = fd[fd.isna().any(axis=1)]
-#             if len(nans):
-#                 vob_vals = list(set([str(v)
-#                                      for v in synth_data.loc[nans.index, f].values.tolist()]))
-#                 vob_features.append((f, vob_vals))
-#                 console_out(f'Value out of bound for feature {f}, '
-#                       f'out of bound values: {vob_vals}. '
-#                             f'Dropping feature from evaluation.')
-#
-#             mask = sd[sd[f] != 'N'].index if has_N else sd.index
-#
-#             if f in ['PINCP'] and not len(nans):
-#                 sd.loc[mask, f] = pd.to_numeric(sd.loc[mask, f])
-#                 sd.loc[mask, f] = sd.loc[mask, f].astype(float)
-#             elif not len(nans):
-#                 sd.loc[mask, f] = pd.to_numeric(sd.loc[mask, f])
-#                 sd.loc[mask, f] = sd.loc[mask, f].astype(int)
-#         elif f == 'PUMA':
-#             # values intersection
-#             f_unique = sd['PUMA'].unique().tolist()
-#             v_intersect = set(f_unique).intersection(set(f_vals))
-#             if len(v_intersect) < len(f_unique):
-#                 vob_vals = list(set([str(v) for v in
-#                                      list(set(f_unique).difference(v_intersect))]))
-#                 vob_features.append((f, vob_vals))
-#                 console_out(f'Value out of bound for feature {f}, '
-#                             f'out of bound values: {vob_vals}. Dropping feature from evaluation.')
-#         else:
-#             fd = sd[[f]].copy()
-#             mask = fd[fd[f] != 'N'].index if has_N else fd.index
-#             fd.loc[mask, f] = pd.to_numeric(fd.loc[mask, f], errors="coerce")
-#             nans = fd[fd.isna().any(axis=1)]
-#             vob_vals = []
-#             if len(nans):
-#                 vob_vals.extend(list(set([str(v)
-#                                           for v in synth_data.loc[nans.index, f].values.tolist()])))
-#                 fd = fd.dropna()
-#
-#             mask = fd[fd[f] != 'N'].index if has_N else fd.index
-#             fd.loc[mask, f] = fd.loc[mask, f].astype(int)
-#
-#             if f != 'INDP':
-#                 real_vals = list(f_vals.keys())
-#                 if 'N' in real_vals:
-#                     real_vals.remove('N')
-#                 f_unique = set(fd.loc[mask, f].unique().tolist())
-#                 real_vals = [int(v) for v in real_vals]
-#                 v_intersect = set(f_unique).intersection(set(real_vals))
-#                 if len(v_intersect) < len(f_unique):
-#                     vob_vals.extend(set([str(v)
-#                                          for v in list(set(f_unique).difference(v_intersect))]))
-#
-#             if len(vob_vals):
-#                 vob_features.append((f, vob_vals))
-#                 console_out(f'Value out of bound for feature {f}, '
-#                       f'out of bound values: {vob_vals}. Dropping feature from evaluation.')
-#             else:
-#                 mask = sd[sd[f] != 'N'].index if has_N else sd.index
-#                 sd.loc[mask, f] = pd.to_numeric(sd.loc[mask, f])
-#                 sd.loc[mask, f] = sd.loc[mask, f].astype(int)
-#         if has_N:
-#             sd[f] = sd[f].astype(object)
-#
-#         if len(vob_features):
-#             last_vob_f, vob_vals = vob_features[-1]
-#             if last_vob_f == f:
-#                 sd = sd.loc[:, sd.columns != f]
-#                 if has_nan:
-#                     vob_features[-1] = (last_vob_f, ['nan'] + list(set(vob_vals)))
-#
-#     validation_log['values_out_of_bound'] = dict(vob_features)
-#     return sd, validation_log
